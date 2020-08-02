fe33a4e5f62b6fdc0bc60bd69a0cb048c7c024b4
jgrizzle <james.grizzle@insight.com>
2020-08-02 17:55:10 -0400

    add tf remote

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,5 +1,11 @@
 terraform {
   backend "azurerm" {
+    storage_account_name = "terraformfilesgrizzle"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_msi              = true
+    subscription_id = "acd2e298-d61f-4ed2-8b28-dc2f60d9f382"
+    tenant_id       = "55a46e16-637a-4cfb-acaa-d89183887b16"
   }
   required_version = ">= 0.12.28"
 }
@@ -8,6 +14,7 @@ provider "azurerm" {
   version = "2.14"
   features {}
 }
+
 
 provider "random" {
   version = "2.3.0"
