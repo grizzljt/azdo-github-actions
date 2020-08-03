cc868f4e918d1000f543ec82d1ee51c4d774eee5
jgrizzle <james.grizzle@insight.com>
2020-08-03 19:03:26 -0400

    adding github action tf reqs

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -10,9 +10,12 @@ terraform {
 
 provider "azurerm" {
   version = "2.14"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
   features {}
 }
-
 
 provider "random" {
   version = "2.3.0"
