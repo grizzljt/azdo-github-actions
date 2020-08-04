1a02d830f62893c86a0be480c545f2f6e5de80e6
jgrizzle <james.grizzle@insight.com>
2020-08-03 20:24:54 -0400

    updating backend config for tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -10,10 +10,10 @@ terraform {
 
 provider "azurerm" {
   version = "2.14"
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  # subscription_id = var.subscription_id
+  # client_id       = var.client_id
+  # client_secret   = var.client_secret
+  # tenant_id       = var.tenant_id
   features {}
 }
 
