63fbe6c577f984d20ee1474722d1e71a4edd0e93
jgrizzle <james.grizzle@insight.com>
2020-08-04 20:13:35 -0400

    updated tf plan and apply

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,11 @@ resource "azurerm_resource_group" "demo" {
   name     = "${var.environment}-${var.app_name}-rg"
   location = var.region_primary
 }
+
+resource "azurerm_container_registry" "demo-acr" {
+  name                     = "demo-acr"
+  resource_group_name      = azurerm_resource_group.demo.name
+  location                 = azurerm_resource_group.demo.location
+  sku                      = "Premium"
+  admin_enabled            = true
+}
