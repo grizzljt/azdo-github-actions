0b25ceea30406f979858faf18aa98e627ecc615d
jgrizzle <james.grizzle@insight.com>
2020-08-06 11:04:38 -0400

    Updated ACR

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "demo" {
 }
 
 resource "azurerm_container_registry" "demo-acr" {
-  name                     = "demoacr"
+  name                     = "grizzleacr27"
   resource_group_name      = azurerm_resource_group.demo.name
   location                 = azurerm_resource_group.demo.location
   sku                      = "Premium"
